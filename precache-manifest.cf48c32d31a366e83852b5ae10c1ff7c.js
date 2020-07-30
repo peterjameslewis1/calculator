213d5566e79fb3b141dacd9627cea03e1897b8f1
Peter Lewis <peterjameslewis4@hotmail.com>
2020-07-30 22:20:40 +0100

    Updates

---
+++ precache-manifest.cf48c32d31a366e83852b5ae10c1ff7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bce509701a1293da9283878265a8666e",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "df4efbcb06b9e726287f",
+    "url": "/calculator/static/css/main.38c7b862.chunk.css"
+  },
+  {
+    "revision": "3bba798d7496970774fc",
+    "url": "/calculator/static/js/2.3b8bffea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.3b8bffea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df4efbcb06b9e726287f",
+    "url": "/calculator/static/js/main.dfc255b8.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
